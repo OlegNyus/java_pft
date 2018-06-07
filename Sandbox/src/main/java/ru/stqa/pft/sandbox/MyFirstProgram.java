2cb5e14aa94d1a8e94b52c6bd8c619dd7c9d9aff
OlegNyus <oleg.nyus@gmail.com>
2018-06-06 21:42:06 -0500

    Added variables

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,11 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.print("Hello, world!");
+    System.out.print("Hello, " + "world !  ");
+
+    int l = 8;
+    int s = l * l;
+    System.out.println("Square with site of " + l + " = " + s);
 
   }
 }
