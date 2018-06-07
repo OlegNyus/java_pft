f6769e554367e362fc4aebdd3ef3850a980f7f7a
OlegNyus <oleg.nyus@gmail.com>
2018-06-06 22:23:03 -0500

    Added a few functions

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,15 +3,31 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 
-  public static void main(String[] args) {
-    String somebody = "world";
-    System.out.print("Hello, " + somebody + " !  ");
 
-    double l = 8.0;
-    double s = l * l;
-    System.out.println("Square with site of " + l + " = " + s);
-    System.out.println("test");
+  public static void main(String[] args) {
+
+   hello("world 1");
+    hello("user");
+    hello("Oleg");
+
+    double l = 5;
+    System.out.println("Sqare area with site " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Area of triangle with site " + a + " and " + b + " = " + area(a, b));
 
   }
-}
 
+  public static void hello(String somebody) {
+    System.out.print("Hello, " + somebody + " ! ");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
+}
