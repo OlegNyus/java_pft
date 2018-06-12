1326df925baa2cda3b16871f26792437733d9cb1
OlegNyus <oleg.nyus@gmail.com>
2018-06-11 21:03:41 -0500

    Added rectangle class

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,19 +3,18 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 
-
   public static void main(String[] args) {
 
-   hello("world 1");
+    hello("world 1");
     hello("user");
     hello("Oleg");
 
     double l = 5;
-    System.out.println("Sqare area with site " + l + " = " + area(l));
+    System.out.println("Area of square with side " + l + " = " + area(l));
 
     double a = 4;
     double b = 6;
-    System.out.println("Area of triangle with site " + a + " and " + b + " = " + area(a, b));
+    System.out.println("Area of triangle with sid e " + a + " and " + b + " = " + area(a, b));
 
   }
 
