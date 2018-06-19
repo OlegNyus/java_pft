b35d42aa54f8972d40753a3429dc88c0f7a61ae9
OlegNyus <oleg.nyus@gmail.com>
2018-06-18 23:02:28 -0500

    Added PointEx class

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,7 +7,7 @@ public class MyFirstProgram {
 
     hello("world 1");
     hello("user");
-    hello("Oleg");
+    hello("Alex");
 
     Square s = new Square(5);
     System.out.println("Area of square with side " + s.l + " = " + s.area());
@@ -15,12 +15,17 @@ public class MyFirstProgram {
     Rectangle r = new Rectangle(4,6);
     System.out.println("Area of triangle with side " + r.a + " and " + r.b + " = " + r.area());
 
+    PointEx d = new PointEx(3, 5);
+    System.out.println("Distance between two PointsEX " + d.p1 + " And " + d.p2 + " = " + Distance(d));
   }
 
   public static void hello(String somebody) {
     System.out.print("Hello, " + somebody + " ! ");
   }
 
+    public static double Distance(PointEx d ){
+      return d.p1 * d.p2;
+    }
 
 
 
---
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/PointEx.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+public class PointEx {
+
+  public double p1;
+  public double p2;
+
+  public PointEx(double p1, double p2) {
+    this.p1 = p1;
+    this.p2 = p2;
+  }
+
+}
